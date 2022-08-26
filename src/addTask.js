0f2f5271c6e9bd0b90746294441d402944f9b524
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2022-08-27 11:54:44 +1200

    Add success sound to addTask

--- src/addTask.js
+++ src/addTask.js
@@ -7,27 +7,32 @@ const notion = new Client({
   auth: config.token,
 });
 
-(async () => {
-  const response = await notion.pages.create({
-    icon: {
-      type: "emoji",
-      emoji: "✉️",
-    },
-    parent: {
-      type: "database_id",
-      database_id: config.database_id,
-    },
-    properties: {
-      Tasks: {
-        title: [
-          {
-            text: {
-              content: alfy.input,
-            },
-          },
-        ],
+const createPage = async () => {
+  notion.pages
+    .create({
+      icon: {
+        type: "emoji",
+        emoji: "✉️",
       },
-    },
-  });
-  console.log(response);
-})();
+      parent: {
+        type: "database_id",
+        database_id: config.database_id,
+      },
+      properties: {
+        Tasks: {
+          title: [
+            {
+              text: {
+                content: alfy.input,
+              },
+            },
+          ],
+        },
+      },
+    })
+    .then(() => {
+      console.log(true);
+    });
+};
+
+createPage();
