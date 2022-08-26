2203cb2ebcd9f77747bc4fd73b32b5725b4bf38c
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2022-08-27 09:47:57 +1200

    Use config.json instead of env.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import alfy from "alfy";
 
-import env from "./env.json" assert { type: "json" };
+import config from "./config.json" assert { type: "json" };
 
-console.log(env.tokenTest);
+console.log(config.token);
