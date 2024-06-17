59bd0c3c76ba3f9c88d5e2431f0a2e10a20abf89
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2024-06-17 16:03:04 +1200

    Update GTD tasks output

--- src/list-options.js
+++ src/list-options.js
@@ -3,38 +3,50 @@ import alfy from "alfy";
 alfy.output([
   {
     title: "Add Task",
-    subtitle: "Add a task to your GTD system",
+    subtitle: `Add "${alfy.input}" to GTD`,
     arg: alfy.input,
     variables: {
       openNotion: false,
       command: "gtdAdd",
+    },
+    icon: {
+      path: "./src/icons/task.png",
     },
   },
   {
     title: "Add Task and Open notion",
-    subtitle: "Add a task to your GTD system and open notion",
+    subtitle: `Add "${alfy.input}" GTD and open Notion`,
     arg: alfy.input,
     variables: {
       openNotion: true,
       command: "gtdAdd",
     },
+    icon: {
+      path: "./src/icons/completed-task.png",
+    },
   },
   {
     title: "Add Reading Task",
-    subtitle: "Add a reading task to your GTD system",
+    subtitle: `Add "${alfy.input}" as a reading task to GTD`,
     arg: alfy.input,
     variables: {
       openNotion: false,
       command: "gtdAddRead",
     },
+    icon: {
+      path: "./src/icons/closed-book.png",
+    },
   },
   {
     title: "Add Reading Task and Open notion",
-    subtitle: "Add a reading task to your GTD system and open notion",
+    subtitle: `Add "${alfy.input}" as a reading task to GTD and open Notion`,
     arg: alfy.input,
     variables: {
       openNotion: true,
       command: "gtdAddRead",
+    },
+    icon: {
+      path: "./src/icons/open-book.png",
     },
   },
 ]);
